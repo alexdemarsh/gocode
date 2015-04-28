998bd16acc062b0248759662b36871613c122307
palex <alexdemarsh@gmail.com>
2015-04-27 21:57:04 -0600

    minor changes

--- webserver-3.py
+++ webserver-3.py
@@ -36,4 +36,65 @@ the html part, and styling just once, then just inject the different blog data i
 
 '''
 
+import socket
+import re
 
+HOST, PORT = '', 8888
+VIEWS_DIR = "./views"
+
+context = {"Title":"This is the title","BlogText":"this is blog data"}
+
+urls = {'/':index_page, '/about':about_page}
+regex = r'###(\w)###'
+
+def read_data(filename):
+	with open(filename, "r") as f:
+		return f.read()
+
+def index_page():
+	return render_template(read_data("views/index.html"))
+
+def about_page():
+	return read_data("views/about.html")
+
+def render_template(template, hash):
+	for x in re.findall(regex, template):
+		html = template.replace('###'+x+'###',hash[x])
+	return html
+
+def run_server():
+    listen_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    listen_socket.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
+    listen_socket.bind((HOST, PORT))
+    listen_socket.listen(1)
+
+ 
+    print 'Serving HTTP on port %s ...' % PORT
+    while True:
+        client_connection, client_address = listen_socket.accept()
+        request = client_connection.recv(4096)
+
+        if not request:
+            continue
+
+        elif f == '/favicon.ico':
+            continue
+
+        firstline = request.split('\r\n')[0]
+        f = firstline.split(' ')[1]        
+
+        if f not in urls:
+        	print "404 - resource not found!"
+        else:
+        	html = urls[f]()
+
+
+        http_response = """\
+    HTTP/1.1 200 OK\r\nContent-Type: text/html\r\n\r\n %s
+    """ % html
+
+        client_connection.sendall(http_response)
+        client_connection.close()
+
+
+run_server()
